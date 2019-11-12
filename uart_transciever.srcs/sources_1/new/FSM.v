d26674aa6d831a188d4ea645f53acfd9a268864f
Jonas <jonckersjonas@gmail.com>
2019-11-12 14:31:50 +0100

    Changed FSM layout and added shiftReg

--- uart_transciever.srcs/sources_1/new/FSM.v
+++ uart_transciever.srcs/sources_1/new/FSM.v
@@ -27,14 +27,13 @@ module FSM(RxD,reset,baud16,valid,midbit,sysclk, shift,state);
     input RxD;
     input midbit;
     output reg valid;
-    output reg shift; 
+    output reg[7:0] shift=8'b00000000;
     output reg [0:3] state;
     parameter IDLE = 4'b0000, STARTBIT =4'b0001, BIT0 = 4'b0010, BIT1 = 4'b0011, BIT2 = 4'b0100, BIT3 = 4'b0101, BIT4 = 4'b0110, BIT5 = 4'b0111,BIT6 = 4'b1000,BIT7 = 4'b1001,STOPBIT = 4'b1010;
     always @(posedge sysclk) begin
         if (reset == 1'b0 )begin
             state <= IDLE;
             valid <= 0;
-            shift <=0;
         end else begin
             if(baud16==1'b1)begin
                 case(state)
@@ -42,35 +41,27 @@ module FSM(RxD,reset,baud16,valid,midbit,sysclk, shift,state);
                 STARTBIT: if(midbit==1'b1) state<=BIT0;
                 BIT0: if(midbit==1'b1)begin 
                     state<=BIT1; 
-                    shift<=RxD;
                     end
                 BIT1: if(midbit==1'b1) begin
                     state<=BIT2;
-                    shift<=RxD;
                     end
                 BIT2: if(midbit==1'b1)begin
-                     shift<=RxD;
                      state<=BIT3;
                      end
                 BIT3: if(midbit==1'b1)begin 
                     state<=BIT4;
-                    shift<=RxD;
                     end
                 BIT4: if(midbit==1'b1)begin 
                     state<=BIT5;
-                    shift<=RxD;
                     end
                 BIT5: if(midbit==1'b1)begin 
                     state<=BIT6;
-                    shift<=RxD;
                     end
                 BIT6: if(midbit==1'b1)begin 
                     state<=BIT7;
-                    shift<=RxD;
                     end
                 BIT7: if(midbit==1'b1)begin 
                     state<=STOPBIT;
-                    shift<=RxD;
                     end
                 STOPBIT:begin 
                        valid <=1 ;
@@ -80,10 +71,11 @@ module FSM(RxD,reset,baud16,valid,midbit,sysclk, shift,state);
                 endcase
             end
         end
-        /*
-        always @(*) begin
-            
-        end
-        */
     end 
+    always @(posedge midbit) begin
+             if(state<=4'b1000 & state>=4'b0000) begin
+                 shift=shift>>1;
+                 shift[7]=RxD;
+             end
+            end
 endmodule
